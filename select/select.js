a86c572f05ec2cd0f5a3258e401cce8b6e92694a
Nikita Kuznetsov <livisback@yandex.ru>
2022-01-17 12:52:47 +0300

    add orivate function

--- select/select.js
+++ select/select.js
@@ -1,3 +1,24 @@
+const getTemplate = () => {
+    return `
+    <div class="select__input">
+                    <span>Text</span>
+                    <i class="fa fa-chevron-down"></i>
+                </div>
+                <div class="select__dropdown">
+                    <ul class="select__list">
+                        <ul class="select__item">123</ul>
+                        <ul class="select__item">123</ul>
+                        <ul class="select__item">123</ul>
+                        <ul class="select__item">123</ul>
+                        <ul class="select__item">123</ul>
+                        <ul class="select__item">123</ul>
+                        <ul class="select__item">123</ul>
+
+                    </ul>
+                </div>
+                `
+}
+
 export class Select {
     constructor(selector, options) {
         this.$el = document.querySelector(selector)
