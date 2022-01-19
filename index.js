27bda921186fb10ae41e461933bc5761d900a6e0
Nikita Kuznetsov <livisback@yandex.ru>
2022-01-19 18:45:31 +0300

    add placeholder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import {Select} from './select/select'
 import './select/styles.scss'
 
 const select = new Select('#select', {
-    
+    placeholder: 'Выбери пожалуйста элемент'
 })
 
 window.s = select
