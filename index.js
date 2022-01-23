d179d809a40c203a86af7f8dac9fe3e4d19a3fe5
Nikita Kuznetsov <livisback@yandex.ru>
2022-01-23 19:35:05 +0300

    add onSelect

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const select = new Select('#select', {
         {id: '4', value: 'React Native'},
         {id: '5', value: 'Next'},
         {id: '6', value: 'Nest'},
-    ]
+    ],
+    onSelect(item) {
+        console.log('Selected item', item)
+    }
 })
 
 window.s = select
