3af9c5b2d3f20c0b3fe15e5c0455f6b782647e69
Nikita Kuznetsov <livisback@yandex.ru>
2022-02-03 23:54:56 +0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './select/styles.scss'
 
 const select = new Select('#select', {
     placeholder: 'Выбери пожалуйста элемент',
-    selectedId: '4',
+    //selectedId: '4',
     data: [
         {id: '1', value: 'React'},
         {id: '2', value: 'Angular'},
